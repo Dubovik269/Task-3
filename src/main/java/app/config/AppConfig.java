6e366bf89d3df1365f03b1ff4116a02df69aa145
egor2 <dubovik269@gmail.com>
2023-06-04 16:05:48 +0300

    пп2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -3,7 +3,9 @@ package app.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = "app")
+@Configuration             //обозначает класс как конфигурационный, который содержит определения бинов
+@ComponentScan(basePackages = "app") // используется для указания пакетов, которые должны быть
+                                     // отсканированы Spring для поиска и автоматического создания бинов
 public class AppConfig {
+
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component // определяет класс как компонент, чтоб спринг созд. его экземпляр и управл. как бином
 public class AnimalsCage {
 
-    @Autowired
+    @Autowired //для автоматического связывания зависимостей
+    @Qualifier("dog") // указывает имя бина, который должен быть связан
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
